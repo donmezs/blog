b9211878e42a4ee660335dd7a6f196562fa3b02b
Suleyman Donmez <ce.suleyman.donmez@gmail.com>
2023-04-14 20:38:59 +0300

    fix: npm build error

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -2,11 +2,11 @@ import React, { FormEvent, useState } from "react";
 import Layout from "@theme/Layout";
 import { FormField } from "@site/src/components/FormField";
 import { NetlifyForm } from "@site/src/components/NetlifyForm";
-import validator from 'validator';
+import validator from "validator";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
   const { siteConfig } = useDocusaurusContext();
@@ -20,42 +20,44 @@ function Contact() {
 
     if (!validator.isEmail(email)) {
       toast.error("Valid email is required", {
-        icon: '👎',
-        theme: 'colored',
-        position: toast.POSITION.BOTTOM_CENTER
+        icon: "👎",
+        theme: "colored",
+        position: toast.POSITION.BOTTOM_CENTER,
       });
       return;
     }
 
     setIsProcessing(true);
 
-    var template = { 
+    var template = {
       name: name,
       email: email,
-      message: message
-   };
+      message: message,
+    };
 
-   var serviceId = siteConfig.customFields.emailJsServiceId as string;
-   var templateId = siteConfig.customFields.emailJsTemplateId as string;
-   var pk = siteConfig.customFields.emailJsPk as string;
+    var serviceId = siteConfig.customFields.emailJsServiceId as string;
+    var templateId = siteConfig.customFields.emailJsTemplateId as string;
+    var pk = siteConfig.customFields.emailJsPk as string;
 
-    emailjs.send(serviceId, templateId, template, pk)
-        .then((result) => {
-          setEmail("");
-          setName("");
-          setMessage("");
-          toast.success("Your message received, thank you!", {
-            icon: '👍',
-            theme: 'colored',
-            position: toast.POSITION.BOTTOM_CENTER
-          });
-        }, (error) => {
-          toast.error("An error occurred. Please try again later.", {
-            icon: '👎',
-            theme: 'colored',
-            position: toast.POSITION.BOTTOM_CENTER
-          });
+    emailjs.send(serviceId, templateId, template, pk).then(
+      (result) => {
+        setEmail("");
+        setName("");
+        setMessage("");
+        toast.success("Your message received, thank you!", {
+          icon: "👍",
+          theme: "colored",
+          position: toast.POSITION.BOTTOM_CENTER,
         });
+      },
+      (error) => {
+        toast.error("An error occurred. Please try again later.", {
+          icon: "👎",
+          theme: "colored",
+          position: toast.POSITION.BOTTOM_CENTER,
+        });
+      }
+    );
 
     setIsProcessing(false);
   }
@@ -66,13 +68,23 @@ function Contact() {
         <div className="row">
           <div className="col col--6 col--offset-3">
             <h1>Contact Me</h1>
-            <p>If you have any comments, ideas, critiques, or you just want to say hi, don’t hesitate to send me an email at 
-              <a href="mailto:sudo@suleymandonmez.dev">sudo@suleymandonmez.dev</a> or fill the form below!</p>
+            <p>
+              If you have any comments, ideas, critiques, or you just want to
+              say hi, don’t hesitate to send me an email at{" "}
+              <a href="mailto:sudo@suleymandonmez.dev">
+                sudo@suleymandonmez.dev
+              </a>{" "}
+              or fill the form below!
+            </p>
             <NetlifyForm onSubmit={submit} name="contact">
-              <FormField label="Your Name" name="name" 
-                        required type="text" 
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}/>
+              <FormField
+                label="Your Name"
+                name="name"
+                required
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
               <FormField
                 fieldType="input"
                 label="Your Email"
@@ -100,7 +112,7 @@ function Contact() {
                   {isProcessing ? "Sending" : "Send"}
                 </button>
               </div>
-              
+
               <ToastContainer />
             </NetlifyForm>
           </div>
